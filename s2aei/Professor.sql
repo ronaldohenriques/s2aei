1e301f22476356f2fbb7905ed7743e1addd9a0f3
ronaldoh <ronaldo_ahenriques@yahoo.com.br>
2016-04-12 21:57:17 -0300

    criado banco

---
+++ s2aei/Professor.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Professor]
+(
+	[idProf] UNIQUEIDENTIFIER IDENTITY,
+	[EmailP] VARCHAR(50) NOT NULL,
+	[senha] VARCHAR(10) NOT NULL,
+	[Nome] VARCHAR(50) NOT NULL,
+	[DisciplinaP] VARCHAR(50) NOT NULL,
+	[Departamento] VARCHAR(50) NOT NULL,
+    CONSTRAINT [PK_Professor] PRIMARY KEY ([idProf])
+)
---
+++ s2aei/Projeto.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Projeto]
+(
+	[idProjeto] UNIQUEIDENTIFIER IDENTITY,
+	[idAlunoLider] UNIQUEIDENTIFIER NOT NULL, 
+    CONSTRAINT [FK_Projeto_AlunoMembro] FOREIGN KEY (idAlunoLider) REFERENCES Participando ([idAluno]), 
+    CONSTRAINT [PK_Projeto] PRIMARY KEY ([IdProjeto])
+
+)
