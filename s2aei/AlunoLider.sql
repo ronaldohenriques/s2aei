1a3c97c68d37d0aaad239cf723623941905b1b18
ronaldoh <ronaldo_ahenriques@yahoo.com.br>
2016-04-12 22:16:57 -0300

    v3

---
+++ s2aei/AlunoLider.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[AlunoLider]
+(
+	[idAlunoLider] UNIQUEIDENTIFIER, 
+	[idProjeto] UNIQUEIDENTIFIER,
+    CONSTRAINT [FK_AlunoLider_AlunoMembro] FOREIGN KEY (idAlunoLider) REFERENCES AlunoMembro(idAluno),
+	CONSTRAINT [FK_AlunoLider_idProjeto] FOREIGN KEY (idProjeto) REFERENCES Projeto(idProjeto) 
+)
