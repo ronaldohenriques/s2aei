441b56babb975548f63e9beba6b5da93130b5697
ronaldoh <ronaldo_ahenriques@yahoo.com.br>
2016-04-12 22:02:36 -0300

    banco

--- s2aei/Participando.sql
+++ s2aei/Participando.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [dbo].[Participando]
 (
 	[idAluno] UNIQUEIDENTIFIER NOT NULL,
-	[idProjeto] UNIQUEIDENTIFIER NOT NULL, 
+	[idProjeto] UNIQUEIDENTIFIER NOT NULL,
     CONSTRAINT [PK_Participando] PRIMARY KEY ([idAluno], [idProjeto]), 
     CONSTRAINT [FK_Participando_AlunoMembro] FOREIGN KEY ([idAluno]) REFERENCES AlunoMembro(idAluno),
 	CONSTRAINT [FK_Participando_Projeto] FOREIGN KEY ([idProjeto]) REFERENCES Projeto(idProjeto)
